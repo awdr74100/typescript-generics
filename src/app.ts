4d799a6a15bc3b40903c49bd6e83aa1dac22e3b5
Yi-Ru Lan <a78945612385238@gmail.com>
2022-07-22 03:52:33 +0800

    Update progress

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,12 @@ console.log(countAndDescribe([]));
 console.log(countAndDescribe(''));
 console.log(countAndDescribe({ length: 20 }));
 // console.log(countAndDescribe(10)); // doesn't work!
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U,
+) {
+  return `Value: ${obj[key]}`;
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
